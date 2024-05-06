96382fed507a8316a07ee3c9348a2998d265a1c5
André <120421201+WedSan@users.noreply.github.com>
2024-05-06 04:28:32 -0300

    refactor: name props name and use it into MPButton component

--- challenge-project/src/components/MainPage/MPButton.tsx
+++ challenge-project/src/components/MainPage/MPButton.tsx
@@ -1,8 +1,8 @@
-interface MPButon{
+interface MPButtonProps{
     name: string;
 }
 
-const MPButton = ({name}) => {
+const MPButton: React.FC<MPButtonProps> = ({name}) => {
     return (
         <button className="bg-white text-secondary-color rounded-md border-2 p-2 border-primary-color
          hover:bg-primary-color hover:text-white transition duration-300 ease-in-out">
