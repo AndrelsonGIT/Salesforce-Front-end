f1769048fd4710bfce401a586a4655cb7da96bb7
WedSan <andre.alves.silvazesez@gmail.com>
2024-05-17 20:09:05 -0300

    refactor: use Link in MainHeroButton directly

--- challenge-project/src/components/buttons/MainHeroButton.tsx
+++ challenge-project/src/components/buttons/MainHeroButton.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 interface MainHeroButtonProps {
-    name:string;
+    name: string;
+    pageToRedirect: string; 
 }
 
-const MainHeroButton: React.FC<MainHeroButtonProps> = ({name}) => {
+const MainHeroButton: React.FC<MainHeroButtonProps> = ({name, pageToRedirect}) => {
     return (
         <button className="border border-primary-color w-3/5 py-1 text-s-gray
          transition-colors duration-300 hover:bg-primary-color hover:text-white
          md:w-64 md:text-lg ">
-            {name}
+            <Link to={pageToRedirect}>{name}</Link>
         </button>
     );
 };
